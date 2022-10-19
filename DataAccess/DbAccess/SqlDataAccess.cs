1f47baf33b3ce5dcd59b26f5161fa83b4b2371f6
CTodorov <CTodorov@EN719572>
2022-10-19 14:20:22 +0300

    Add project files.

---
+++ DataAccess/DbAccess/SqlDataAccess.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DbAccess;
+
+public class SqlDataAccess : ISqlDataAccess
+{
+	private readonly IConfiguration _configuration;
+
+	public SqlDataAccess(IConfiguration configuration)
+	{
+		_configuration = configuration;
+	}
+
+	public async Task<IEnumerable<T>> LoadData<T, U>(string storedProcedures,
+												  U parameters,
+												  string connectionId = "Default")
+	{
+        using IDbConnection connection = new SqlConnection(_configuration.GetConnectionString(connectionId));
+
+        return await connection.QueryAsync<T>(storedProcedures, parameters,
+            commandType: CommandType.StoredProcedure);
+    }
+
+	public async Task SaveData<T>(string storedProcedures,
+												  T parameters,
+												  string connectionId = "Default")
+	{
+		using IDbConnection connection = new SqlConnection(_configuration.GetConnectionString(connectionId));
+
+		await connection.ExecuteAsync(storedProcedures, parameters, commandType: CommandType.StoredProcedure);
+	}
+}
---
+++ MinimalAPIDemoTimCorey/GlobalUsings.cs
@@ -0,0 +1,3 @@
+﻿// file for the global usings so that its not necessery to write them in Program.cs
+global using DataAccess.Data;
+global using DataAccess.Models;
